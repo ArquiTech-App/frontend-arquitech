55e98ffdd351b7cd8fd0ad1bcd5c8cef26b3fe5b
HugoRo <ndhugohro@gmail.com>
2022-04-23 03:05:06 -0500

    adding all pages of the front

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ import Image from 'next/image'
 
 export default function Home () {
   return (
+    <div>
+      <h1>THE REPORTING APP FOR THE BEST ARCHITECTURE PROJECTS
+      </h1>
 
-    <h1> sample text</h1>
+    </div>
+
   )
 }
---
+++ pages/view_3d.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function view_3d () {
+  return (
+    <div>view_3d</div>
+  )
+}

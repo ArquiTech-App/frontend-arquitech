0de0922d9587c7e0047145aff401604436df085d
deveckor <deveckor@gmail.com>
2022-04-25 23:26:47 -0500

    Add page login

--- components/login-input.js
+++ components/login-input.js
@@ -4,8 +4,18 @@ export default function LoginInput(){
 
     return (
         <>
-        <section>
-            
+        <section className="input-login">
+            <h3>Welcome to Arquitetch</h3>
+            <p>Login</p>
+            <form className="form-login" id="login">
+                <label>Email
+                    <input name="email" type="email"/>
+                </label>
+                <label>Password
+                    <input name="password" type="password"/>
+                </label>
+                <input className="btn-login" type="submit" value="Login"/>
+            </form>
         </section>
         </>
     )
--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,27 @@
 import LoginInput from '../components/login-input'
-
+import Image from 'next/image'
+import logo from '../public/logo.svg'
 
 export default function Login(){
 
 
 
     return(
-        <div className="background">
-        <LoginInput/>
+        <div className="login-page">
+            <div className="login-div">
+
+            </div>
+            
+            <div className="login-div-two">
+            <Image 
+            src={logo}
+            alt="Logo"
+            width={150}
+            height={150}
+            />
+            <LoginInput/>
+            </div>
+            
         </div>
     )
 }

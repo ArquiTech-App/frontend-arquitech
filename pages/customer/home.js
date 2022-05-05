e6e44801d5e8af87622ae4648cb71b4c8ba66636
Cristian Luna <cristianluru@gmail.com>
2022-05-04 21:22:14 -0500

    adding card

--- pages/customer/home.js
+++ pages/customer/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import Link from 'next/link'
+
 
 export default function home() {
   return (
       <Layout>
-
+        <Link href="/CustomerHome"> Dashboard Client </Link>
           
       </Layout>
   )

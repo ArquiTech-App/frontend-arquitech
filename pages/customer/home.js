7f10ea0966d294b090568e49ba3415dec9a9d773
Cristian Luna <cristianluru@gmail.com>
2022-05-09 22:09:22 -0500

    merge

--- pages/customer/home.js
+++ pages/customer/home.js
@@ -5,17 +5,10 @@ import Link from 'next/link'
 
 export default function home() {
   return (
-<<<<<<< HEAD
       <LayoutCustomers>
         <Link href="/CustomerHome"> Dashboard Client </Link>
           
       </LayoutCustomers>
-=======
-      <Layout>
-
-          <h3>hola</h3>
-      </Layout>
->>>>>>> d21312a2c736f79c37cfaea8847e0263edde0cae
   )
 }
 

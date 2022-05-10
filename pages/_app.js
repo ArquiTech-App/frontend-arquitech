d1d9aae3ff49438961166bc87d2b203d37914752
deveckor <deveckor@gmail.com>
2022-05-10 17:12:35 -0500

    add dynamic component

--- pages/_app.js
+++ pages/_app.js
@@ -7,13 +7,6 @@ import {OfficeProvider} from '../context/officeContext'
 
 function MyApp({ Component, pageProps }) {
 
-  if (typeof window !== 'undefined') {
-    console.log('You are on the browser')
-    // ✅ Can use window here
-  } else {
-    console.log('You are on the server')
-    // ⛔️ Don't use window here
-  }
   
   return (
 <OfficeProvider>

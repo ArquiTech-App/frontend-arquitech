6ec1315a72e97eeba9cf08772dcdd3895c13b7ff
deveckor <deveckor@gmail.com>
2022-05-13 08:13:54 -0500

    add changes

--- components/AsideOffice.js
+++ components/AsideOffice.js
@@ -101,7 +101,7 @@ export default function AsideOffice() {
                         <ListGroup>
                         <ListGroup.Item>
                           <Link href={{
-                            pathname: "documents",
+                            pathname: "/offices/documents",
                             query:{
                               id: el._id
                             }
@@ -122,7 +122,7 @@ export default function AsideOffice() {
                             </button>
                           </Link>
                           <Link href={{
-                            pathname: "viewerOffice",
+                            pathname: "/offices/viewerOffice",
                             query:{
                               id: el._id
                             }

60433a474fc74931d0390399d250d43e662afeed
Héctor Enríquez <94152013+Deveckor@users.noreply.github.com>
2022-05-13 19:05:56 -0500

    Update vieweroffice.js

--- pages/offices/vieweroffice.js
+++ pages/offices/vieweroffice.js
@@ -146,7 +146,7 @@ export default function Viewer(){
                   return(
                     
                     
-                    <option key={id}  value={i}>{el.name}</option>
+                    <option key={i}  value={i}>{el.name}</option>
                     
                     
                     
